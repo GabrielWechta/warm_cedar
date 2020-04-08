450fb3198a963302e9cd3794d7f0ee593ae92f92
GabrielWechta <wechtagabriel@gmail.com>
2020-04-08 14:31:48 +0200

    numpy.fft?

--- rok 2/kurs_pythona/250111/List_4/BigNumbers.py
+++ rok 2/kurs_pythona/250111/List_4/BigNumbers.py
@@ -1,13 +1,16 @@
 from cmath import exp
 from math import pi
 
+import numpy as np
+from numpy.fft import fft, ifft
+
 
 class FastBigNum(object):
     def __init__(self, str_number):
         self.str_number = str_number
         self.vec_number = self.strToVec(str_number)
         self.trans_vec_number = self.dft(self.vec_number, 2 * len(self.vec_number))
-        print(self.vec_number)
+        # print(self.vec_number)
         print(self.trans_vec_number)
 
     def __str__(self):
@@ -37,15 +40,39 @@ class FastBigNum(object):
 
     def strToVec(self, str_number):
         vector = [int(s) for s in str_number]
-        print(vector)
         return vector
 
 
-A = "123123123123123123123123123123123123123123123123123"
-B = "123412341234123412341234123412341234123412341234123"
+#############################################################
 
-a = FastBigNum(A)
-b = FastBigNum(B)
+class NumpyFastBigNum(object):
+    def __init__(self, str_number):
+        self.str_number = str_number
+        self.vec_number = self.strToVec(str_number)
+        self.trans_vec_number = fft(self.vec_number)
+        print(self.trans_vec_number)
 
-print(a * b)
-print(123123123123123123123123123123123123123123123123123 * 123412341234123412341234123412341234123412341234123 )
+    def __str__(self):
+        sum = 0
+        for i in range(len(self.vec_number) - 1):
+            sum += self.vec_number[i] * 10 ** (len(self.vec_number) - i - 2)
+        return str(sum)
+
+    def __mul__(self, other):
+        z = []
+        for i in range(len(self.trans_vec_number)):
+            z.append(self.trans_vec_number[i] * other.trans_vec_number[i])
+        self.trans_vec_number = z
+
+        self.vec_number = ifft(self.trans_vec_number)
+        return self
+
+    def strToVec(self, str_number):
+        vector = [int(s) for s in str_number]
+        return vector
+
+a = NumpyFastBigNum("12")
+print()
+b = NumpyFastBigNum("23")
+
+print(np.fft.fft(np.exp(2j * np.pi * np.arange(1) / 1)))
--- rok 2/kurs_pythona/250111/List_4/zad5.py
+++ rok 2/kurs_pythona/250111/List_4/zad5.py
@@ -1,38 +1,12 @@
 from cmath import exp
 from math import pi
+from BigNumbers import FastBigNum
 
-def vecToInt(z):
-    sum = 0
-    for i in range(len(z) - 1):
-        sum += z[i]*10**(len(z) - i - 2)
-    return sum
+A = "123123123123123123123123123123123123123123123123123"
+B = "123412341234123412341234123412341234123412341234123"
 
+a = FastBigNum(A)
+b = FastBigNum(B)
 
-def omega(k, n):
-    return exp(-2j * k * pi / n)
-
-
-def dft(x, n):
-    return [sum(x[i] * omega(i * k, n) if i < len(x) else 0 for i in range(n)) for k in range(n)]
-
-
-def idft(x, n):
-    return [int(round(sum(x[i] * omega(-i * k, n) if i < len(x) else 0 for i in range(n)).real) / n) for k in range(n)]
-
-
-x = dft([1, 2], 4)
-y = dft([1, 1], 4)
-z = []
-for i in range(4):
-    z.append(x[i] * y[i])
-
-print(x)
-print(y)
-print(z)
-
-z = idft(z, 4)
-
-print(z)
-
-z = vecToInt(z)
-print(z)
+print(a * b)
+print(123123123123123123123123123123123123123123123123123 * 123412341234123412341234123412341234123412341234123 )
