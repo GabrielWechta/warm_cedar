0197c749cab94dd94427910a83bcfca4b77f60cc
GabrielWechta <wechtagabriel@gmail.com>
2020-12-31 01:59:40 +0100

    doing ON

--- rok 3/obliczenia_naukowe/lista_5/blocksys.jl
+++ rok 3/obliczenia_naukowe/lista_5/blocksys.jl
@@ -1,220 +1,261 @@
+module blocksys
+
+export read_matrix_from_file, read_right_side_vector_from_file, gauss, gauss_choosing_main_element, solve_gauss, solve_gauss_with_main_element, do_lu, do_lu_with_main_element, solve_lu, solve_lu_with_main_element
+
 using SparseArrays
 
-function read_matrix_from_file(file_path::String)
-    open(file_path, "r") do file # opening file
-        tmp_line = split(readline(file), " ") # first line - n and l
-        n = parse(Int64, tmp_line[1])
-        l = parse(Int64, tmp_line[2])
+    function read_matrix_from_file(file_path::String)
+        open(file_path, "r") do file # opening file
+            tmp_line = split(readline(file), " ") # first line - n and l
+            n = parse(Int64, tmp_line[1])
+            l = parse(Int64, tmp_line[2])
 
-        A = spzeros(Float64, n, n) # creating forbidden fruit
+            A = spzeros(Float64, n, n) # creating forbidden fruit
 
-        for line in eachline(file) # parsing values
-            vars = split(line, " ")
-            i = parse(Int64, vars[1])
-            j = parse(Int64, vars[2])
-            value = parse(Float64, vars[3])
+            for line in eachline(file) # parsing values
+                vars = split(line, " ")
+                i = parse(Int64, vars[1])
+                j = parse(Int64, vars[2])
+                value = parse(Float64, vars[3])
 
-            A[j, i] = value
+                A[j, i] = value
+            end
+            return (A, n, l)
         end
-        return (A, n, l)
     end
-end
 
-function read_right_side_vector_from_file(file_path::String)
-    open(file_path, "r") do file # opening file
-        n = parse(Int64, readline(file)) # first line - n
+    function read_right_side_vector_from_file(file_path::String)
+        open(file_path, "r") do file # opening file
+            n = parse(Int64, readline(file)) # first line - n
 
+            b = Vector{Float64}(undef, n)
+            i = 1
+
+            for line in eachline(file)
+                b[i] = parse(Float64, line)
+                i += 1
+            end
+            return (b, n)
+        end
+    end
+
+    function gauss(A::SparseMatrixCSC{Float64, Int64}, b::Vector{Float64}, n::Int64, l::Int64, doing_LU::Bool)
+        for k = 1:n-1 # k is for rows
+            how_many_to_eliminate = k + (l - k % l) # optimazing
+
+            for i = k + 1:how_many_to_eliminate # i is for columns
+                multiplier = A[k, i] / A[k, k]
+
+                if doing_LU == true
+                    A[k, i] = multiplier
+                else
+                    A[k, i] = Float64(0.0)
+                end
+
+                for j = k + 1:min(k+l, n)
+                    A[j, i] -= multiplier * A[j, k]
+                end
+
+                if doing_LU == false
+                    b[i] -= multiplier * b[k]
+                end
+            end
+        end
+        return A, b
+    end
+
+    function solve_gauss(A::SparseMatrixCSC{Float64, Int64}, b::Vector{Float64}, n::Int64, l::Int64)
+        A, b = gauss(A, b, n, l, false) # getting from gauss
+
+        x = Vector{Float64}(undef, n) # answer vector
+
+        for i = n:-1:1 # starting from end of A
+            sum = Float64(0.0)
+
+            for j = i + 1:min(n, i + l)
+                sum += A[j, i] * x[j]
+            end
+            x[i] = (b[i] - sum) / A[i, i]
+        end
+
+        return x
+    end
+
+    function gauss_choosing_main_element(A::SparseMatrixCSC{Float64, Int64}, b::Vector{Float64}, n::Int64, l::Int64, doing_LU::Bool)
+        permutation = Vector{Int64}(undef, n)
+
+        # initializing permutation vector
+        # it will be used to "rotate" vector - b
+        for i = 1:n
+            permutation[i] = i
+        end
+
+        for k = 1:n-1 # k is for rows
+            how_many_to_eliminate = k + (l - k % l) # optimazing
+            row = k # row is for finding max element
+            max_element = abs(A[k, k]) # variable for biggest element in columns
+
+            for i = k:how_many_to_eliminate
+                if abs(A[k, permutation[i]]) > max_element
+                    max_element = abs(A[k, permutation[i]])
+                    row = i
+                end
+            end
+
+            # saving permutation
+            permutation[k], permutation[row] = permutation[row], permutation[k]
+
+            # standard part + fixing based on permutation
+            for i = k + 1:how_many_to_eliminate
+                multiplier = A[k, permutation[i]] / A[k, permutation[k]]
+
+                if doing_LU == true
+                    A[k, permutation[i]] = multiplier
+                else
+                    A[k, permutation[i]] = Float64(0.0)
+                end
+
+                for j = k + 1:min(k + 2 * l, n)
+                    A[j, permutation[i]] -= multiplier * A[j, permutation[k]]
+                end
+
+                if doing_LU == false
+                    b[permutation[i]] -= multiplier * b[permutation[k]]
+                end
+            end
+        end
+        return (A, permutation, b)
+    end
+
+    function solve_gauss_with_main_element(A::SparseMatrixCSC{Float64, Int64}, b::Vector{Float64}, n::Int64, l::Int64)
+        A, permutation, b = gauss_choosing_main_element(A, b, n, l, false)
+
+        x = Vector{Float64}(undef, n) # answer vector
+
+        for i = n:-1:1 # i is for rows
+            sum = Float64(0.0)
+            for j = i + 1:min(i + 2 * l + 1, n) # j is for columns
+                sum += A[j, permutation[i]] * x[j]
+            end
+
+            x[i] = (b[permutation[i]] - sum) / A[i, permutation[i]] # calculating x based on permuatation
+        end
+        return x
+    end
+
+    function do_lu(A::SparseMatrixCSC{Float64, Int64}, b::Vector{Float64}, n::Int64, l::Int64)
+        return gauss(A, b, n, l, true)
+    end
+
+    function do_lu_with_main_element(A::SparseMatrixCSC{Float64, Int64}, b::Vector{Float64}, n::Int64, l::Int64)
+         return gauss_choosing_main_element(A, b, n, l, true)
+    end
+
+    function solve_lu(A::SparseMatrixCSC{Float64, Int64}, b::Vector{Float64}, n::Int64, l::Int64)
+
+        """ Part for changing b """
+        modified_b = Vector{Float64}(undef, n) # for modified b
+        for i = 1:n
+            sum = Float64(0.0)
+
+            for j = max(1, Int64(l * floor((i - 1) / l))):i - 1 # from Kincaid
+                sum += A[j, i] * modified_b[j]
+            end
+
+            modified_b[i] = b[i] - sum
+        end
+
+        """ Part for calculating x """
+        x = Vector{Float64}(undef, n) # answer vector
+        for i = n:-1:1
+            sum = Float64(0.0)
+
+            for j = i + 1:min(n, i + l + 1)
+                sum += A[j, i] * x[j]
+            end
+            x[i] = (modified_b[i] - sum) / A[i, i]
+        end
+
+        return x
+    end
+
+    function solve_lu_with_main_element(A::SparseMatrixCSC{Float64, Int64}, permutation::Vector{Int64},
+                                                    b::Vector{Float64}, n::Int64, l::Int64)
+
+        """ Part for changing b """
+        modified_b = Vector{Float64}(undef, n)
+        for i = 1:n
+            sum = Float64(0.0)
+
+            for j = max(1, Int64(l * floor((i - 1) / l)) - 1):i - 1
+                sum += A[j, permutation[i]] * modified_b[j]
+            end
+
+            modified_b[i] = b[permutation[i]] - sum
+        end
+
+        """ Part for calculating x """
+        x = Vector{Float64}(undef, n)
+        for i = n:-1:1
+            sum = Float64(0.0)
+
+            for j = i + 1:min(i + 2 * l + 1, n)
+                sum += A[j, permutation[i]] * x[j]
+            end
+
+            x[i] = (modified_b[i] -  sum) / A[i, permutation[i]]
+        end
+
+        return x
+    end
+
+    function save_results_to_file(file_path::String, x::Vector{Float64}, n::Int64, vec_b_generated::Bool)
+        open(file_path, "w") do file
+            """ calculating error if b was generated """
+            if vec_b_generated == true
+                ones = ones(n)
+
+                err = norm(ones - x) / norm(x)
+                println(file, err)
+            end
+
+            for i = 1:n
+                println(file, x[i])
+            end
+        end
+    end
+
+    function calculate_vector_b(A::SparseMatrixCSC{Float64, Int64}, n::Int64, l::Int64)
         b = Vector{Float64}(undef, n)
-        i = 1
 
-        for line in eachline(file)
-            b[i] = parse(Float64, line)
-            i += 1
-        end
-        return (b, n)
-    end
-end
+        for i = 1:n
+            b[i] = Float64(0.0)
 
-function gauss(A::SparseMatrixCSC{Float64, Int64}, b::Vector{Float64}, n::Int64, l::Int64, doing_LU::Bool)
-    for k = 1:n-1 # k is for rows
-        how_many_to_eliminate = k + (l - k % l) # optimazing
-
-        for i = k + 1:how_many_to_eliminate # i is for columns
-            multiplier = A[k, i] / A[k, k]
-
-            if doing_LU == true
-                A[k, i] = multiplier
-            else
-                A[k, i] = Float64(0.0)
+            for j = max(1, Int64(l * floor((i - 1) / l)) - 1):min(n, Int64(l + l * floor((i - 1) / l)))
+                b[i] += A[j, i]
             end
 
-            for j = k + 1:min(k+l, n)
-                A[j, i] -= multiplier * A[j, k]
-            end
-
-            if doing_LU == false
-                b[i] -= multiplier * b[k]
-            end
-        end
-    end
-    return A, b
-end
-
-function solve_gauss(A::SparseMatrixCSC{Float64, Int64}, b::Vector{Float64}, n::Int64, l::Int64)
-    A, b = gauss(A, b, n, l, false) # getting from gauss
-
-    x = Vector{Float64}(undef, n) # answer vector
-
-    for i = n:-1:1 # starting from end of A
-        sum = Float64(0.0)
-
-        for j = i + 1:min(n, i + l)
-            sum += A[j, i] * x[j]
-        end
-        x[i] = (b[i] - sum) / A[i, i]
-    end
-
-    return x
-end
-
-function gauss_choosing_main_element(A::SparseMatrixCSC{Float64, Int64}, b::Vector{Float64}, n::Int64, l::Int64, doing_LU::Bool)
-    permutation = Vector{Int64}(undef, n)
-
-    # initializing permutation vector
-    # it will be used to "rotate" vector - b
-    for i = 1:n
-        permutation[i] = i
-    end
-
-    for k = 1:n-1 # k is for rows
-        how_many_to_eliminate = k + (l - k % l) # optimazing
-        row = k # row is for finding max element
-        max_element = abs(A[k, k]) # variable for biggest element in columns
-
-        for i = k:how_many_to_eliminate
-            if abs(A[k, permutation[i]]) > max_element
-                max_element = abs(A[k, permutation[i]])
-                row = i
+            if i + l <= n
+                b[i] += A[i, i + l]
             end
         end
 
-        # saving permutation
-        permutation[k], permutation[row] = permutation[row], permutation[k]
-
-        # standard part + fixing based on permutation
-        for i = k + 1:how_many_to_eliminate
-            multiplier = A[k, permutation[i]] / A[k, permutation[k]]
-
-            if doing_LU == true
-                A[k, permutation[i]] = multiplier
-            else
-                A[k, permutation[i]] = Float64(0.0)
-            end
-
-            for j = k + 1:min(k + 2 * l, n)
-                A[j, permutation[i]] -= multiplier * A[j, permutation[k]]
-            end
-
-            if doing_LU == false
-                b[permutation[i]] -= multiplier * b[permutation[k]]
-            end
-        end
+        return b
     end
-    return (A, permutation, b)
 end
 
-function solve_gauss_with_main_element(A::SparseMatrixCSC{Float64, Int64}, b::Vector{Float64}, n::Int64, l::Int64)
-    A, permutation, b = gauss_choosing_main_element(A, b, n, l, false)
-
-    x = Vector{Float64}(undef, n) # answer vector
-
-    for i = n:-1:1 # i is for rows
-        sum = Float64(0.0)
-        for j = i + 1:min(i + 2 * l + 1, n) # j is for columns
-            sum += A[j, permutation[i]] * x[j]
-        end
-
-        x[i] = (b[permutation[i]] - sum) / A[i, permutation[i]] # calculating x based on permuatation
-    end
-    return x
-end
-
-function do_lu(A::SparseMatrixCSC{Float64, Int64}, b::Vector{Float64}, n::Int64, l::Int64)
-    return gauss(A, b, n, l, true)
-end
-
-function do_lu_with_main_element(A::SparseMatrixCSC{Float64, Int64}, b::Vector{Float64}, n::Int64, l::Int64)
-     return gauss_choosing_main_element(A, b, n, l, true)
-end
-
-function solve_lu(A::SparseMatrixCSC{Float64, Int64}, b::Vector{Float64}, n::Int64, l::Int64)
-
-    """ Part for changing b """
-    modified_b = Vector{Float64}(undef, n) # for modified b
-    for i = 1:n
-        sum = Float64(0.0)
-
-        for j = max(1, Int64(l * floor((i - 1) / l))):i - 1 # from Kincaid
-            sum += A[j, i] * modified_b[j]
-        end
-
-        modified_b[i] = b[i] - sum
-    end
-
-    """ Part for calculating x """
-    x = Vector{Float64}(undef, n) # answer vector
-    for i = n:-1:1
-        sum = Float64(0.0)
-
-        for j = i + 1:min(n, i + l + 1)
-            sum += A[j, i] * x[j]
-        end
-        x[i] = (modified_b[i] - sum) / A[i, i]
-    end
-
-    return x
-end
-
-function solve_lu_with_main_element(A::SparseMatrixCSC{Float64, Int64}, permutation::Vector{Int64},
-                                                b::Vector{Float64}, n::Int64, l::Int64)
-
-    """ Part for changing b """
-    modified_b = Vector{Float64}(undef, n)
-    for i = 1:n
-        sum = Float64(0.0)
-
-        for j = max(1, Int64(l * floor((i - 1) / l)) - 1):i - 1
-            sum += A[j, permutation[i]] * modified_b[j]
-        end
-
-        modified_b[i] = b[permutation[i]] - sum
-    end
-
-    """ Part for calculating x """
-    x = Vector{Float64}(undef, n)
-    for i = n:-1:1
-        sum = Float64(0.0)
-
-        for j = i + 1:min(i + 2 * l + 1, n)
-            sum += A[j, permutation[i]] * x[j]
-        end
-
-        x[i] = (modified_b[i] -  sum) / A[i, permutation[i]]
-    end
-
-    return x
-end
-
-
-A, n, l = read_matrix_from_file("/home/gabriel/Desktop/studia/rok 3/obliczenia_naukowe/lista_5/dane16/A.txt")
-A_sec, n, l = read_matrix_from_file("/home/gabriel/Desktop/studia/rok 3/obliczenia_naukowe/lista_5/dane16/A.txt")
-
-b, n = read_right_side_vector_from_file("/home/gabriel/Desktop/studia/rok 3/obliczenia_naukowe/lista_5/dane16/b.txt")
-b_sec, n = read_right_side_vector_from_file("/home/gabriel/Desktop/studia/rok 3/obliczenia_naukowe/lista_5/dane16/b.txt")
-
-A1, perm,  b1 = do_lu_with_main_element(A,b,n,l)
-x = solve_lu_with_main_element(A1, perm, b1, n, l)
+#
+# A, n, l = read_matrix_from_file("/home/gabriel/Desktop/studia/rok 3/obliczenia_naukowe/lista_5/dane16/A.txt")
+# A_sec, n, l = read_matrix_from_file("/home/gabriel/Desktop/studia/rok 3/obliczenia_naukowe/lista_5/dane16/A.txt")
+#
+# b, n = read_right_side_vector_from_file("/home/gabriel/Desktop/studia/rok 3/obliczenia_naukowe/lista_5/dane16/b.txt")
+# b_sec, n = read_right_side_vector_from_file("/home/gabriel/Desktop/studia/rok 3/obliczenia_naukowe/lista_5/dane16/b.txt")
+#
+# new_b = calculate_vector_b(A, n, l)
+# A1, perm,  b1 = do_lu_with_main_element(A,b,n,l)
+# x = solve_lu_with_main_element(A1, perm, b1, n, l)
 # A2, b2 = gauss(A_sec,b_sec,n,l, true)
 # mat_1 = Matrix(A1)
 # mat_2 = Matrix(A2)
 # x = solve_gauss_with_main_element(A, b, n, l)
+# save_results_to_file("/home/gabriel/Desktop/studia/rok 3/obliczenia_naukowe/lista_5/result.txt", x, n, false)
---
+++ rok 3/obliczenia_naukowe/lista_5/tests.jl
@@ -0,0 +1,2 @@
+using blocksys
+using matrixgen
