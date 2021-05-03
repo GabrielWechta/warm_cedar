f6c126424dbc68d7b822825a63eb12fb72b9e8ba
GabrielWechta <wechtagabriel@gmail.com>
2021-05-03 13:30:24 +0200

    last commit before moving out <3 "

--- rok_3/systemy_wbudowane/vhdl/lista_4/crc8_tb.vhd
+++ rok_3/systemy_wbudowane/vhdl/lista_4/crc8_tb.vhd
@@ -1,3 +1,5 @@
+LIBRARY STD;
+USE STD.textio.ALL;
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
@@ -39,6 +41,10 @@ ARCHITECTURE behavior OF crc8_tb IS
 	SIGNAL data_out_66 : STD_LOGIC_VECTOR(7 DOWNTO 0);
 	-- access address
 	SIGNAL address : STD_LOGIC_VECTOR(2 DOWNTO 0) := (OTHERS => '0');
+
+	SIGNAL ok_a0 : STD_LOGIC := '0';
+	SIGNAL ok_66 : STD_LOGIC := '0';
+
 BEGIN
 	-- Instantiate the Unit Under Test (UUT)
 	uut : crc8 PORT MAP(
@@ -76,14 +82,81 @@ BEGIN
 	END PROCESS;
 	-- Stimulus process
 	stim_proc : PROCESS
+		VARIABLE Xa0 : STD_LOGIC_VECTOR(7 DOWNTO 0) := ('1', '0', '1', '0', '0', '0', '0', '0');
+		VARIABLE X66 : STD_LOGIC_VECTOR(7 DOWNTO 0) := ('0', '1', '1', '0', '0', '1', '1', '0');
+
+		VARIABLE zero : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
+		VARIABLE address_holder : STD_LOGIC_VECTOR(2 DOWNTO 0) := ('0', '0', '1');
+		VARIABLE address_zero : STD_LOGIC_VECTOR(2 DOWNTO 0) := (OTHERS => '0');
+
 	BEGIN
+		data_in <= Xa0;
 		WAIT ON clk;
-		address <= STD_LOGIC_VECTOR(unsigned(address) + 1);
-		data_in <= data_out_a0;
-		WAIT FOR clk_period * 10;
+
+		FOR I IN 0 TO 7 LOOP
+			WAIT FOR clk_period;
+
+			address <= address_holder;
+			address_holder := STD_LOGIC_VECTOR(unsigned(address_holder) + 1);
+
+		END LOOP;
+
+		--restarting
 		data_in <= crc_out;
+		WAIT FOR clk_period;
+
+		address_holder := address_zero;
+		address <= address_holder;
+
+		data_in <= X66;
+
+		FOR I IN 0 TO 7 LOOP
+			address <= address_holder;
+			address_holder := STD_LOGIC_VECTOR(unsigned(address_holder) + 1);
+			WAIT FOR clk_period;
+
+		END LOOP;
+
+		-- graph flating 
+		LOOP
+			data_in <= crc_out;
+			WAIT FOR clk_period;
+		END LOOP;
 
 		WAIT;
+	END PROCESS;
+
+	-- Clock process definitions
+	ok_process : PROCESS
+		VARIABLE ok_a0_v : STD_LOGIC := '0';
+		VARIABLE ok_66_v : STD_LOGIC := '0';
+		VARIABLE zero : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
+
+		VARIABLE my_line : line;
+	BEGIN
+		WAIT FOR 100 ps;
+
+		IF crc_out = data_out_a0
+			THEN
+			ok_a0_v := '1';
+		ELSE
+			ok_a0_v := '0';
+		END IF;
+
+		IF crc_out = data_out_66
+			THEN
+			ok_66_v := '1';
+		ELSE
+			ok_66_v := '0';
+		END IF;
+
+		ok_a0 <= ok_a0_v;
+		ok_66 <= ok_66_v;
+
+		IF crc_out /= zero AND (ok_a0_v /= '1' OR ok_66_v /= '1') and THEN
+			write(my_line, STRING'("Hello World")); -- formatting
+			writeline(output, my_line);
+		END IF;
 	END PROCESS;
 
 END;
