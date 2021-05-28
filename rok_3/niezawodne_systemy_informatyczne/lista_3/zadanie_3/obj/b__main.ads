a9f9714134ea316b93507863141008906ee0a02b
GabrielWechta <wechtagabriel@gmail.com>
2021-05-28 13:01:44 +0200

    first part of webowe

--- rok_3/niezawodne_systemy_informatyczne/lista_3/zadanie_3/obj/b__main.ads
+++ rok_3/niezawodne_systemy_informatyczne/lista_3/zadanie_3/obj/b__main.ads
@@ -37,7 +37,7 @@ package ada_main is
    pragma Export (C, main, "main");
 
    type Version_32 is mod 2 ** 32;
-   u00001 : constant Version_32 := 16#c2180c08#;
+   u00001 : constant Version_32 := 16#07e6cc33#;
    pragma Export (C, u00001, "mainB");
    u00002 : constant Version_32 := 16#67c8d842#;
    pragma Export (C, u00002, "system__standard_libraryB");
@@ -263,7 +263,7 @@ package ada_main is
    pragma Export (C, u00112, "system__finalization_rootS");
    u00113 : constant Version_32 := 16#bbaa76ac#;
    pragma Export (C, u00113, "system__file_control_blockS");
-   u00114 : constant Version_32 := 16#92e8c563#;
+   u00114 : constant Version_32 := 16#07f14787#;
    pragma Export (C, u00114, "fB");
    u00115 : constant Version_32 := 16#54da27e6#;
    pragma Export (C, u00115, "system__img_lluB");
--- rok_3/niezawodne_systemy_informatyczne/lista_3/zadanie_3/src/f.adb
+++ rok_3/niezawodne_systemy_informatyczne/lista_3/zadanie_3/src/f.adb
@@ -1,12 +1,21 @@
 with Interfaces; use Interfaces;
--- Collatz
+with Ada.Text_IO; use Ada.Text_IO;
+
+-- Collatz shortcut
 function F (X : Unsigned_64) return Unsigned_64 with SPARK_Mode is
-   Y : Unsigned_64 := X;
+   Y       : Unsigned_64 := X;
+   Bin_Rep : Unsigned_64 := X with Ghost;
 begin
    while Y mod 2 /= 0 loop
-      Y := (3 * Y + 1) / 2;
+      pragma Assert (if Bin_Rep mod 2 /= 0 then Y mod 2 /= 0);
+
       pragma Loop_Invariant (3 * Y + 1 <= Unsigned_64'Last * 2);
-      pragma Loop_Variant (Decreases => Unsigned_64'Last - (3 * Y + 1) / 2);
+
+      pragma Loop_Variant (Decreases => Bin_Rep); -- Y - (Y and (Y + 1)), (not Y) and (Y + 1)
+
+      Y := (3 * Y + 1) / 2;
+      Bin_Rep := Bin_Rep / 2;
+
    end loop;
    return Y / 2;
 end F;
