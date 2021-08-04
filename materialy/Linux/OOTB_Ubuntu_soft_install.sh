20a8dc9c7eddaa613d4c75eb0c3516e6b4296b91
GabrielWechta <wechtagabriel@gmail.com>
2021-08-04 10:57:29 +0200

    basic isntall script

---
+++ materialy/Linux/OOTB_Ubuntu_soft_install.sh
@@ -0,0 +1,41 @@
+# Repositories
+apt update
+apt upgrade
+
+# Basic Software
+snap install git
+
+# Python
+apt install python3-pip
+snap install jupyter
+snap install pycharm-professional
+
+# Java
+apt install openjdk-18-jre
+
+# Editors
+snap install code --classic
+snap install notepad-plus-plus
+
+# Media
+snap install gimp
+snap install vlc
+
+# Social
+snap install rambox
+snap install slack
+snap install spotify
+apt-get install mendeleydesktop
+
+# Gnome
+add-apt-repository universe
+apt install gnome-tweak-tool
+
+# Firewall
+apt install gufw -y
+
+# Cleaner
+add-apt-repository ppa:gerardpuig/ppa
+apt-get update
+apt-get install ubuntu-cleaner
+
