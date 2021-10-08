ee962dbf4482448cd4193e2d4552808c160d0e0a
GabrielWechta <wechtagabriel@gmail.com>
2021-10-08 18:43:28 +0200

    new on OOTB install

--- materialy/Linux/OOTB_Ubuntu_soft_install.sh
+++ materialy/Linux/OOTB_Ubuntu_soft_install.sh
@@ -13,6 +13,14 @@ snap install pycharm-professional --classic
 # Java
 apt install default-jre -y
 
+# Web Browser - Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P /tmp
+dpkg -i /tmp/google-chrome-stable_current_amd64.deb
+
+# Microsoft Teams
+wget https://packages.microsoft.com/repos/ms-teams/pool/main/t/teams/teams_1.3.00.5153_amd64.deb -P /tmp
+dpkg -i /tmp/teams_1.3.00.5153_amd64.deb
+
 # Editors
 snap install code --classic
 snap install notepad-plus-plus
@@ -20,6 +28,7 @@ snap install notepad-plus-plus
 # Media
 snap install gimp
 snap install vlc
+apt-get install nomacs -y
 
 # Social
 snap install rambox
