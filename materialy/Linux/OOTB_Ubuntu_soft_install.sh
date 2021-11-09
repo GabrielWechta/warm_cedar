bd68418800e9a0f76c51d763320d78a00bb0de66
Gabriel Wechta <wechtagabriel@gmail.com>
2021-11-09 09:39:34 +0100

    Added Signal

--- materialy/Linux/OOTB_Ubuntu_soft_install.sh
+++ materialy/Linux/OOTB_Ubuntu_soft_install.sh
@@ -53,6 +53,14 @@ snap install rambox
 snap install slack
 snap install spotify
 
+# Signal
+cd /tmp
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+sudo apt update && sudo apt install signal-desktop
+cd 
+
 # Mendeley
 # visit this site: https://www.mendeley.com/guides/download-mendeley-desktop/ubuntu/instructions
 # then: dpkg -i mendeleydesktop_1.19.8-stable_amd64.deb 
