d431584bba6f0b3b53093220bccb3779623817fd
Gabriel Wechta <wechtagabriel@gmail.com>
2021-11-22 18:44:12 +0100

    bye TP

--- rok_2/technologie_programowania/list_3/paterns_project/zad2/builder/src/main/java/eu/jpereira/trainings/designpatterns/creational/builder/ReportBuilder.java
+++
@@ -1,9 +0,0 @@
-package eu.jpereira.trainings.designpatterns.creational.builder;
-
-import eu.jpereira.trainings.designpatterns.creational.builder.model.Report;
-import eu.jpereira.trainings.designpatterns.creational.builder.model.SaleEntry;
-
-public interface ReportBuilder {
-	public Report getReport();
-	public void setSaleEntry(SaleEntry saleEntry);
-}
